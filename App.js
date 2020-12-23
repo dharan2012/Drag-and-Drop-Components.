c07a548a6ff6c08dcc7b4f02fbf1fcc2f8a70c0a
dharan2012 <muralidharangk@gmail.com>
2020-12-23 14:57:13 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,78 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  PanResponder,
+  Animated
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Draggable from "./Draggable";
+
+
+const App: () => React$Node = () => {
+  return (
+
+    <View style={styles.mainContainer}>
+        <View style={styles.dropZone}>
+          <Text style={styles.text}>Drop them here!</Text>
+        </View>
+        <View style={styles.ballContainer} />
+        <View style={styles.row}>
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+        </View>
+      </View>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  ballContainer: {
+    height:200
+  },
+  row: {
+    flexDirection: "row"
+  },  
+  dropZone: {
+    height: 200,
+    backgroundColor: "#00334d"
+  },
+  text: {
+    marginTop: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold"
+  }
+});
+
+
+export default App;
